df0de97e093760d679a23a22da8c66f49acce40c
rohan mathur <rohanmathur@rohans-MacBook-Air.local>
2022-12-26 20:09:43 +0530

    first commit

---
+++ backend_sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'jy3lfyv6',
+    dataset: 'production'
+  }
+})
---
+++ backend_sanity/schemas/index.ts
@@ -0,0 +1,10 @@
+import testimonials from './testimonials';
+import abouts from './abouts';
+import brands from './brands';
+import contact from './contact';
+import experiences from './experiences';
+import workExperience from './workExperience';
+import skills from './skills';
+import works from './works';
+
+export const schemaTypes = [ testimonials, abouts, brands, contact, experiences, workExperience, skills, works ]
